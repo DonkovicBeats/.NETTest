102c02feba5fb516acd243acb85dbed41d4ff829
DonkovicBeats <eliasrnhardt@gmail.com>
2025-11-26 12:53:22 +0100

    Filling MxM Board

--- ErsteDotNETApp/Program.cs
+++ ErsteDotNETApp/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("Hallo, jetzt ist " + DateTime.Now);
+﻿const int WIDTH = 4;
+const int HEIGHT = WIDTH;
+
+char[,] board = new char[WIDTH, HEIGHT];
+
+
+Console.Clear();
+
+
+System.Console.WriteLine("+++++++Start 1+++++++");
+for (int i = 0; i < HEIGHT; i++)
+{
+
+    //System.Console.Write("\n");
+    for (int j = 0; j < WIDTH; j++)
+    {
+        board[i, j] = 'X';
+        PrintBoard(board, i, j);
+        Thread.Sleep(150);
+
+    }
+    System.Console.Write("\n");
+
+
+}
+
+
+System.Console.WriteLine("+++++++End+++++++");
+
+void PrintBoard(char[,] inp, int row, int col)
+{
+    System.Console.Write(inp[row, col]);
+
+}
