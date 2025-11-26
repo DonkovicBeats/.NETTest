a0d1b7a743b960e21f81d3872d7a26e52e45de25
DonkovicBeats <eliasrnhardt@gmail.com>
2025-11-26 17:51:42 +0100

    initialized ps1 and ps2

--- ErsteDotNETApp/Person.cs
+++ ErsteDotNETApp/Person.cs
@@ -2,5 +2,28 @@
 
 public class Person
 {
-    
+    private int height;
+
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    public Person(int height)
+    {
+        Height = height;
+        Console.WriteLine($"Person with Height of: {Height}cm initialized.");
+    }
+   
 }
--- ErsteDotNETApp/Program.cs
+++ ErsteDotNETApp/Program.cs
@@ -1,4 +1,6 @@
-﻿const int WIDTH = 4;
+﻿using ErsteDotNETApp;
+
+const int WIDTH = 4;
 const int HEIGHT = WIDTH;
 
 char[,] board = new char[WIDTH, HEIGHT];
@@ -6,8 +8,18 @@ char[,] board = new char[WIDTH, HEIGHT];
 
 Console.Clear();
 
+try
+{
+    Person ps = new Person(140);
+    Person ps3 = new Person(178);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Person is too young!");
+} 
 
-System.Console.WriteLine("+++++++Start 1+++++++");
+
+/*System.Console.WriteLine("+++++++Start 1+++++++");
 
 for (int i = 0; i < HEIGHT; i++)
 {
@@ -35,5 +47,5 @@ System.Console.WriteLine("+++++++End+++++++");
 void PrintBoard(char[,] inp, int row, int col)
 {
     System.Console.Write(inp[row, col]);
+*/
 
-}
