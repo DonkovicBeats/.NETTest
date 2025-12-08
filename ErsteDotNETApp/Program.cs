317738df1ee5d99758bb03a581ec469d1c14dfd5
DonkovicBeats <eliasrnhardt@gmail.com>
2025-12-08 16:59:14 +0100

    idk anymore atp

--- ErsteDotNETApp/Program.cs
+++ ErsteDotNETApp/Program.cs
@@ -8,45 +8,6 @@ char[,] board = new char[WIDTH, HEIGHT];
 
 Console.Clear();
 
-/*try
-{
-    Person ps = new Person("Barry");
-}
-catch (ArgumentOutOfRangeException e)
-{
-    Console.WriteLine("Person is too young!");
-}
-catch (ArgumentException e)
-{
-    Console.WriteLine("Person could not be initialized!");
-}*/
-
-
-/*System.Console.WriteLine("+++++++Start 1+++++++");
-
-for (int i = 0; i < HEIGHT; i++)
-{
-
-    //System.Console.Write("\n");
-    for (int j = 0; j < WIDTH; j++)
-    {
-        board[i, j] = 'X';
-        PrintBoard(board, i, j);
-        Thread.Sleep(150);
-
-    }
-    System.Console.Write("\n");
-
-
-}
-
-
-
-
-
-
-System.Console.WriteLine("+++++++End+++++++");
-
 void PrintBoard(char[,] inp, int row, int col)
 {
     System.Console.Write(inp[row, col]);
